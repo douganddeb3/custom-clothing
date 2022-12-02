52230e1dd9c3ba88079b26c9dd718bfb618df375
douganddeb3 <douganddeb3@msn.com>
2022-12-02 09:55:40 -0500

    add context to cart

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Home from './routes/home/home.component';
 
 import Shop from './routes/shop/shop.component';
 
+import Checkout from './routes/checkout/checkout.component';
+
 
 const App = () => {
   return(
@@ -15,10 +17,11 @@ const App = () => {
        <Route index element={<Home />} />
        <Route path='shop' element={<Shop />} />
        <Route path='auth' element={<Authentication />} />
-
+       <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );  
 };
 
 export default App;
+
