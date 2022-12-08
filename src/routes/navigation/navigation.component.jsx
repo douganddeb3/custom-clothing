387e116028f68d3eb477c01ebebe7e2248c19cc1
douganddeb3 <douganddeb3@msn.com>
2022-12-08 14:49:39 -0500

    redirect file for netlify 2

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,7 @@
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { Fragment, useContext } from 'react';
-import {Outlet, Link} from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 import { ReactComponent as CrwnLogo} from '../../assets/crown.svg';
 import {UserContext} from '../../contexts/user.context';
 
