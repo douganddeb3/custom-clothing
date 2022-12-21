9199676ad0623167c3b99b5f8d5861168ca9997c
douganddeb3 <douganddeb3@msn.com>
2022-12-21 12:32:46 -0500

    right after thunk done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 // for redux, Provider is redux version of Provider which uses context:
 import { Provider } from 'react-redux';
-import {store } from './store/store';
+
+import { PersistGate } from 'redux-persist/integration/react';
+
+import {store, persistor } from './store/store';
 
 import App from './App';
 // import {UserProvider} from './contexts/user.context';
@@ -17,9 +20,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-              <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+                <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,16 +2,17 @@ import {useState, useEffect, Fragment} from 'react';
 import {useSelector} from 'react-redux';
 import {useParams} from 'react-router-dom';
 
-import {selectCategoriesMap} from '../../store/categories/category.selector';
+import {selectCategoriesIsLoading, selectCategoriesMap} from '../../store/categories/category.selector';
 
 import ProductCard from '../../components/product-card/product-card.component';
-
+import Spinner from '../../components/spinner/spinner.component';
 import './category.styles.scss';
 // import { connectFirestoreEmulator } from 'firebase/firestore';
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const [products, setProducts] = useState(categoriesMap[category]);
     console.log('render/re-rendering category component');
     console.log('effect firing setProducts is next.');
@@ -22,11 +23,16 @@ const Category = () => {
     return(
         <Fragment>
             <h2 className='category-title'>{category.toUpperCase()}</h2>
-            <div className='category-container'>
-            {   products &&
-                products.map((product) => <ProductCard key={product.id} product={product} />)
+            {
+                isLoading ? <Spinner />
+                :
+                <div className='category-container'>
+                    {   products &&
+                        products.map((product) => <ProductCard key={product.id} product={product} />)
+                    }
+            </div> 
             }
-            </div>  
+             
         </Fragment>
         
     );
